6057458869be89ef2bcf39af97a3cb43c377ff64
JVCarv41 <jvc.04.off@gmail.com>
2025-06-23 23:03:36 -0300

    feature: added a file with the basic test configurations, such as obtaining the token for any requests that require authentication

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from app import create_app
+from config import Config
+
+@pytest.fixture(scope="module")
+def flask_app():
+    app = create_app(Config)
+    app.config['TESTING'] = True
+    yield app
+
+@pytest.fixture(scope="module")
+def flask_client(flask_app):
+    return flask_app.test_client()
+
+
+@pytest.fixture(scope="module")
+def artesao_token(flask_client):
+    response = flask_client.post('/auth/login', json={
+        'email': "artesao@teste.com",
+        'senha': '1234'
+    })
+    assert response.status_code == 200
+    return response.json['token']

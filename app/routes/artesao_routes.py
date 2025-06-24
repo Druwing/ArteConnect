aa2ba6c6decbbfb8fedc418c80e0399387f4019d
JVCarv41 <jvc.04.off@gmail.com>
2025-06-23 22:42:28 -0300

    fix: Added authentication to the routes for artesao and carrinho

--- app/routes/artesao_routes.py
+++ app/routes/artesao_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
-from app.controllers.artesao_controller import cadastrar_artesao, obter_artesao, listar_artesaos
+from app.controllers.artesao_controller import obter_artesao, listar_artesaos
+from app.controllers.auth_controller import login_required
 
 bp = Blueprint('artesaos', __name__, url_prefix='/artesaos')
 
-bp.route('/', methods=['POST'])(cadastrar_artesao)
-bp.route('/', methods=['GET'])(listar_artesaos)
-bp.route('/<string:artesao_id>', methods=['GET'])(obter_artesao)
+bp.route('/', methods=['GET'])(login_required(listar_artesaos))
+bp.route('/<string:artesao_id>', methods=['GET'])(login_required(obter_artesao))
--- app/routes/carrinho_routes.py
+++ app/routes/carrinho_routes.py
@@ -2,11 +2,12 @@ from flask import Blueprint
 from app.controllers.carrinho_controller import (
     ver_carrinho, adicionar_ao_carrinho, remover_do_carrinho, limpar_carrinho, checkout
 )
+from app.controllers.auth_controller import login_required
 
 bp = Blueprint('carrinho', __name__, url_prefix='/carrinho')
 
-bp.route('/', methods=['GET'])(ver_carrinho)
-bp.route('/adicionar', methods=['POST'])(adicionar_ao_carrinho)
-bp.route('/remover', methods=['POST'])(remover_do_carrinho)
-bp.route('/limpar', methods=['POST'])(limpar_carrinho)
-bp.route('/checkout', methods=['POST'])(checkout)
+bp.route('/', methods=['GET'])(login_required(ver_carrinho))
+bp.route('/adicionar', methods=['POST'])(login_required(adicionar_ao_carrinho))
+bp.route('/remover', methods=['POST'])(login_required(remover_do_carrinho))
+bp.route('/limpar', methods=['POST'])(login_required(limpar_carrinho))
+bp.route('/checkout', methods=['POST'])(login_required(checkout))

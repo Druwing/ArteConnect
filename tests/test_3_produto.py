6f462e9dab6fa3dc66eec53c2e56b14d6306d237
JVCarv41 <jvc.04.off@gmail.com>
2025-06-26 19:40:37 -0300

    test: merged test_8_into test_3_ which now verifies if a client can manipulate the products (it must not be able to)

--- tests/test_3_produto.py
+++ tests/test_3_produto.py
@@ -1,6 +1,8 @@
 import pytest
 
-# ----------- SUCCESS CASES -----------
+# =====================================
+# ======= TESTES BEM-SUCEDIDOS ========
+# =====================================
 def test_criar_produto_success(flask_client, artesao_token):
     response = flask_client.post('/produtos/', json={
         'nome': 'Produto Teste',
@@ -28,8 +30,12 @@ def test_atualizar_quantidade_produto_success(flask_client, artesao_token):
     }, headers={"Authorization": f"Bearer {artesao_token}"})
     assert response.status_code == 200
     assert 'Quantidade do produto atualizada com sucesso' in response.json['message']
+    
+# =====================================
+# =========== TESTES FALHOS ===========
+# =====================================
 
-# ----------- FAIL CASES -----------
+# -------- no auth --------
 
 def test_criar_produto_fail_no_auth(flask_client):
     response = flask_client.post('/produtos/', json={
@@ -58,3 +64,33 @@ def test_remover_produto_fail_no_auth(flask_client):
 def test_remover_todos_produtos_fail_no_auth(flask_client):
     response = flask_client.post('/produtos/remover_todos')
     assert response.status_code == 401
+    
+# -------- client auth --------
+def test_criar_produto_fail_client_auth(flask_client, cliente_token):
+    response = flask_client.post('/produtos/', json={
+        'nome': 'Produto Teste',
+        'descricao': 'Descrição do produto',
+        'preco': 100.50,
+        'quantidade': 5
+    }, headers={"Authorization": f"Bearer {cliente_token}"})
+    assert response.status_code == 403
+
+def test_atualizar_quantidade_produto_fail_client_auth(flask_client, cliente_token):
+    global produto_id
+    response = flask_client.post('/produtos/atualizar_quantidade', json={
+        'produto_id': produto_id,
+        'quantidade': 2
+    }, headers={"Authorization": f"Bearer {cliente_token}"})
+    assert response.status_code == 403
+
+def test_remover_produto_fail_client_auth(flask_client, cliente_token):
+    global produto_id
+    response = flask_client.post('/produtos/remover', json={
+        'produto_id': produto_id
+    }, headers={"Authorization": f"Bearer {cliente_token}"})
+    assert response.status_code == 403
+
+def test_remover_todos_produtos_fail_client_auth(flask_client, cliente_token):
+    response = flask_client.post('/produtos/remover_todos',
+                                 headers={"Authorization": f"Bearer {cliente_token}"})
+    assert response.status_code == 403
